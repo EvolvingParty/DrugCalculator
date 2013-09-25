07169b607d0bd6678df0b7dfaa5b2e145ac6ceac
Kurt <kurtlane@live.com.au>
2013-09-26 05:43:01 +1000

    v1.1

---
+++ Drug Calc v1.1/QAS Drug Calculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  QAS Drug Calculator
+//
+//  Created by Kurt Lane on 25/08/13.
+//  Copyright (c) 2013 kurtlane.com.au. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Drug Calc v1.1/QAS Drug CalculatorTests/QAS_Drug_CalculatorTests.h
@@ -0,0 +1,13 @@
+//
+//  QAS_Drug_CalculatorTests.h
+//  QAS Drug CalculatorTests
+//
+//  Created by Kurt Lane on 25/08/13.
+//  Copyright (c) 2013 kurtlane.com.au. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface QAS_Drug_CalculatorTests : SenTestCase
+
+@end
